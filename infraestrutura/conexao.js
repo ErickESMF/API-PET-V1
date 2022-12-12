7a7a8d46b139ddb9fe4d401524c3bed8f2ae68b8
Erick Feitosa <103589391+ErickESMF@users.noreply.github.com>
2022-12-12 15:03:39 -0300

    Update conexao.js

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -4,7 +4,7 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'erickvanussy-2015',
+    password: 'senha',
     database: 'agenda-petshop'
 })
 
